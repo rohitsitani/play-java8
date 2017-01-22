121b0cd29ac5a5954687372cfad4d70f2dbf8bce
Rohit Sitani <rohitsitani@gmail.com>
2017-01-22 23:37:15 +0800

    use of java.util.function.Function functional interface

--- src/com/rohit/lambda/MyLambdaInterface.java
+++ src/com/rohit/lambda/MyLambdaInterface.java
@@ -1,5 +1,7 @@
 package com.rohit.lambda;
 
+import java.util.function.Function;
+
 //@FunctionalInterface
 public interface MyLambdaInterface {
 	
@@ -11,14 +13,17 @@ public interface MyLambdaInterface {
 	
 	public static void main(String[] args) {
 		MyLambdaInterface sum = (int a, int b) -> {  return (b + a); };
+		System.out.println("sum:" + sum.calculate(3, 4));
 		MyLambdaInterface diff = (int a, int b) -> {  return (b - a); };
+		System.out.println("diff:" + diff.calculate(3, 4));
 		MyLambdaInterface multiply = (int a, int b) -> {  return (b * a); };
+		System.out.println("multiply:" + multiply.calculate(3, 4));
 		//here the actual calculation is done by a normal method which has no lambda awareness
 		//here that method is passed as a parameter and then the rest of the anonymous class with methods is auto generated
 		MyLambdaInterface max = Math::max;
-		System.out.println("sum:" + sum.calculate(3, 4));
-		System.out.println("diff:" + diff.calculate(3, 4));
-		System.out.println("multiply:" + multiply.calculate(3, 4));
 		System.out.println("max:" + max.calculate(3, 4));
+		//using the general Function class of the new function package in java 
+		Function<Integer, Integer> abc = Math::abs;
+		System.out.println("function.apply:" + abc.apply(-111));
 	}
 }
