d2d82cd5a6e62e8555c7b9090a2c7edf8b26b3d0
Rohit Sitani <rohitsitani@gmail.com>
2017-01-22 23:53:20 +0800

    use of lambda expression with definition using static method call

--- src/com/rohit/lambda/MyLambdaInterface.java
+++ src/com/rohit/lambda/MyLambdaInterface.java
@@ -19,10 +19,13 @@ public interface MyLambdaInterface {
 		System.out.println("diff:" + diff.calculate(3, 4));
 		MyLambdaInterface multiply = (int a, int b) -> {  return (b * a); };
 		System.out.println("multiply:" + multiply.calculate(3, 4));
+		//using the Math class
+		MyLambdaInterface maxLambda = (int a, int b) -> { return Math.max(a, b);};
+		System.out.println("max:" + maxLambda.calculate(3, 4));
 		//here the actual calculation is done by a normal method which has no lambda awareness
 		//here that method is passed as a parameter and then the rest of the anonymous class with methods is auto generated
-		MyLambdaInterface max = Math::max;
-		System.out.println("max:" + max.calculate(3, 4));
+		MyLambdaInterface maxLambdaMethod = Math::max;
+		System.out.println("max:" + maxLambdaMethod.calculate(3, 4));
 		//using the general Function class of the new function package in java 
 		Function<Integer, Integer> function = Math::abs;
 		System.out.println("function.apply:" + function.apply(-111));
